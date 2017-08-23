e43fca29e9bdaea5695a0f0b23d318da95e057b9
Frank <>
2017-08-23 19:09:01 +0800

    测试Fragment

---
+++ app/src/main/java/com/davis/fragment/IUpdateListener.java
@@ -0,0 +1,11 @@
+package com.davis.fragment;
+
+/**
+ * @author Davis
+ * @Description:
+ * @date 2017/8/23 下午3:51
+ * @copyright
+ */
+public interface IUpdateListener {
+    void update(String content);
+}
