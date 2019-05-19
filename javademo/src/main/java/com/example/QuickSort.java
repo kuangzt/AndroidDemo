bb8531ae02403402abe1de8f744af3b8919283fe
Frank <>
2019-05-19 16:39:06 +0800

    【QuickSort】

--- javademo/src/main/java/com/example/QuickSort.java
+++ javademo/src/main/java/com/example/QuickSort.java
@@ -3,23 +3,28 @@ package com.example;
 import java.util.Arrays;
 
 
-public class QuickSortTest {
-    public static void  main(String[] args){
-        System.out.println("main");
-        int[] data = new int[]{10,5,8,6,3,89,66,32,0,0,0,0,0,0,89,88};
-        quickSort(data,0,data.length-1);
-        System.out.println("sort"+ Arrays.toString(data));
+//public static void  main(String[] args){
+//        System.out.println("main");
+//        int[] data = new int[]{10,5,8,6,3,89,66,32,0,0,0,0,0,0,89,88};
+//        quickSort(data,0,data.length-1);
+//        System.out.println("sort"+ Arrays.toString(data));
+//        }
+public final class QuickSort {
+
+
+    private QuickSort() {
+
     }
 
     public static void quickSort(int[] data,int start,int end){
         if(start<end){
-            int privotIndex = quickSortPartition(data,start,end);
+            int privotIndex = selectPrivot(data,start,end);//选择主元
             quickSort(data,start,privotIndex-1);
             quickSort(data,privotIndex+1,end);
         }
     }
 
-    private static int quickSortPartition(int[] data,int start,int end){
+    private static int selectPrivot(int[] data, int start, int end){
         int low = start+1;
         int high = end;
         int privot = data[start];
